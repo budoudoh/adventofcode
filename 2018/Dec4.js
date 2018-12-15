8528746368ef6095e742061e67455219c74ce4ac
Basil Udoudoh <budoudoh@gmail.com>
2018-12-15 09:18:50 -0500

    Dec4

--- 2018/Dec4.js
+++ 2018/Dec4.js
@@ -29,7 +29,7 @@ rl.on('close', function(){
         sort_b = createSortValue(b);
         return sort_a - sort_b;
     });
-    getGuardandTime(records);
+    console.log(getGuardandTime(records));
 });
 
 
@@ -46,8 +46,72 @@ function getGuardandTime(records){
     var guardRegex = /Guard #(.*?) begins shift$/;
     var sleepRegex = /falls asleep$/;
     var wakeRegex = /wakes up$/;
-    var sleepRecords = [];
-    for(record in records){
-
+    var sleepRecords = {};
+    var id;
+    var index = 0;
+    for(var record of records){
+        if(guardRegex.exec(record.text) != null){
+            id = guardRegex.exec(record.text)[1];
+            if(!sleepRecords.hasOwnProperty(id)){
+                sleepRecords[id] = {
+                    sleepHours: {},
+                    count: 0
+                }
+            }
+            index = 0;
+        }
+        else if(sleepRegex.exec(record.text) != null){
+            index = record.minute;
+        }
+        else if(wakeRegex.exec(record.text) != null){
+            for(var i = index; i < record.minute; i++){
+                if(!sleepRecords[id].sleepHours.hasOwnProperty(i)){
+                    sleepRecords[id].sleepHours[i] = [];
+                }
+                sleepRecords[id].sleepHours[i].push(record.month+"/"+record.day);
+                sleepRecords[id].count = sleepRecords[id].count + 1;
+            }
+            
+        }
     }
+return getMaxMinuteGuard(sleepRecords);
+
+}
+
+function getMaxGuardMinute(sleepRecords){
+    var maxCount = 0;
+    var maxId;
+    for(var record in sleepRecords){
+        if(sleepRecords[record].count > maxCount){
+            maxCount = sleepRecords[record].count;
+            maxId = record;
+        }
+    }
+
+    var maxHourCount = 0;
+    var maxHour;
+    for(var hour in sleepRecords[maxId].sleepHours){
+        if(sleepRecords[maxId].sleepHours[hour].length > maxHourCount){
+            maxHourCount = sleepRecords[maxId].sleepHours[hour].length;
+            maxHour = hour;
+        }
+    }
+    return maxId +":"+ maxHour;
+}
+
+function getMaxMinuteGuard(sleepRecords){
+    var maxId;
+    var maxHourCount = 0;
+    var maxHour;
+    for(var record in sleepRecords){
+        for(var hour in sleepRecords[record].sleepHours){
+            if(sleepRecords[record].sleepHours[hour].length > maxHourCount){
+                maxHourCount = sleepRecords[record].sleepHours[hour].length;
+                maxId = record;
+                maxHour = hour;
+            }
+        }
+        
+    }
+    return maxId +":"+ maxHour;
 }
