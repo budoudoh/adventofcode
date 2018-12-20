7dd43c227948b303f1f83a36485dfa9aa68f3d20
Basil Udoudoh <budoudoh@gmail.com>
2018-12-19 23:52:12 -0500

    Dec9

--- 2018/Dec9.js
+++ 2018/Dec9.js
@@ -1,2 +1,29 @@
-var playerCount;
-var lastMarbleValue;
+var playerCount = 10;
+var lastMarbleValue = 1618;
+
+var scores = [];
+for(var i= 0; i < playerCount; i++){
+    scores.push(0);
+}
+
+var circle = [];
+var currentIndex = 0;
+var currentPlayer = 0;
+for(var i =0; i <= lastMarbleValue; i++){
+    
+    if(i%23 == 0){
+        var pullIndex;
+        if(currentIndex < 7){
+            pullIndex = circle.length - 1 - 7 - currentIndex;
+        }
+        else{
+            pullIndex = currentIndex - 7;
+        }
+        var pullValue = circle.splice(pullIndex, 1);
+        scores[currentPlayer] = scores[currentPlayer] + pullValue + i;
+    }
+    currentPlayer++
+    if(currentPlayer == playerCount){
+        currentPlayer = 0;
+    }
+}
