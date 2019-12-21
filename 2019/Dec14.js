bf13f9457fa2c2d2df36d2561404bce74b555ff3
Basil Udoudoh <budoudoh@gmail.com>
2019-12-21 11:42:48 -0500

    Dec14 further work

--- 2019/Dec14.js
+++ 2019/Dec14.js
@@ -16,39 +16,90 @@ rl.on('line', function (line) {
         var components = layout[0].trim().split(",");
         for(var i = 0; i < components.length; i++){
             var chemical = parseChemical(components[i]);
-            reaction.components.push({
-                checmical: chemical.chemical,
-                units: chemical.units
-            });
+            reaction.components.push(chemical);
         }
         reactions[parts.chemical] = reaction;
     }
 });
 
 rl.on('close', function(){
-    console.log(reactions);
+    console.log(chemicalCost(reactions, 1, "FUEL", "ORE"));
 });
 
 function chemicalCost(reactions, units, endChemical, startChemical){
-    var checmicals = {}
+    var chemicals = {}
     var chain = [];
-    var checmical = {
+    var chemical = {
         compound: endChemical,
         units: units
     };
-    chain.push(checmical);
+    chain.push(chemical);
 
     while(chain.length > 0){
-        checmical = chain.pop();
-        var components = reactions[checmical.compound].components;
-        var units = reactions[checmical.compound].units;
+        chemical = chain.pop();
+        if(reactions.hasOwnProperty(chemical.compound)){
+            var components = reactions[chemical.compound].components;
+            var formula_units = reactions[chemical.compound].units;
+            var factor;
+            var extra;
+            if(chemical.units > formula_units){
+                factor = Math.ceil(chemical.units/formula_units);
+                extra = factor*formula_units - chemical.units;
+
+                if(chemicals.hasOwnProperty(chemical.compound)){
+                    var potential = (factor - 1)*formula_units + chemicals[chemical.compound].extra;
+                    if(potential >= chemical.units){
+                        var difference = potential - chemical.units;
+                        factor = factor - 1;
+                        chemicals[chemical.compound].extra = chemicals[chemical.compound].extra - difference;
+                    }
+                    else{
+                        chemicals[chemical.compound].extra = chemicals[chemical.compound].extra + extra;
+                    }
+                    chemicals[chemical.compound] = chemicals[chemical.compound] + chemical.units;
+                }
+                else{
+                    chemicals[chemical.compound] = {
+                        units: chemical.units,
+                        extra: extra
+                    }
+                }
+            }
+            for(var i = 0; i < components.length; i++){
+                var current  = {
+                    compound: components[i].chemical,
+                    units: components[i].units*factor
+                }
+                chain.push(current);
+            }
+        }
+        else if(chemicals.hasOwnProperty(chemical.compound)){
+            chemicals[chemical.compound].units = chemicals[chemical.compound].units + chemical.units;
+            
+        }
+        else{
+            chemicals[chemical.compound] = {
+                units: chemical.units,
+                extra: 0
+            }
+        }
+
     }
-    return checmicals[startChemical];
+    return chemicals;
 }
+
 function parseChemical(chemical){
     var parse = chemical.trim().split(" ");
     return {
         chemical: parse[1].trim(),
         units: parseInt(parse[0].trim())
     }
+}
+
+function findFactor(input, base){
+    var x = 1;
+    while(base*x < input){
+        x++;
+    }
+    return x;
 }
